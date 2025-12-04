f05786429fa4c6b6d43e58460b6a05936ac6843c
Otto Peräkylä <otto.perakyla@gmail.com>
2025-12-04 11:27:50 +0200

    init

--- src/set.js
+++ src/set.js
@@ -1,25 +1,87 @@
 class MySet {
-    constructor(...vals) {
+    constructor(vals) {
         this.dict = {}
         for (let i = 0; i < vals.length; i++) {
-            this.dict[vals] = true
+            this.add(vals[i])
         }
     }
 
     vals() {
         return Object.keys(this.dict)
     }
+    
+    has(key) {
+        return this.dict[key] == true
+    }
+
+    add(key) {
+        this.dict[key] = true
+    }
+
+    delete(key) {
+        let res = set()
+        let vvals = this.vals()
+
+        for (let i = 0; i < vvals.length; i++) {
+            if (vvals[i] != key) {
+                res.add(vvals[i])
+            }
+        }
+        
+        return res
+    }
+
+    union(b) {
+        let res = set()
+        let avals = this.vals()
+        let bvals = b.vals()
+        
+        for (let i = 0; i < avals.length; i++) {
+            res.add(avals[i])
+        }
+
+        for (let i = 0; i < bvals.length; i++) {
+            res.add(bvals[i])
+        }
+
+        return res
+    }
+
+    intersection(b) {
+        let res = set()
+        let bvals = b.vals()
+
+        for (let i = 0; i < bvals.length; i++) {
+            if (this.has(bvals[i])) {
+                res.add(bvals[i])
+            }
+        }
+        
+        return res
+    }
+
+    difference(b) {
+        let res = set()
+        let avals = this.vals()
+        
+        for (let i = 0; i < avals.length; i++) {
+            if (!b.has(avals[i])) {
+                res.add(avals[i])
+            }
+        }
+
+        return res
+    }
+
+    symmetricDifference(b) {
+        return this.union(b).difference(this.intersection(b))
+    }
 }
 
-function set() {
-    return new MySet()
-}
-
-function symmetricDifference() {
-    return null
+function set(...vals) {
+    return new MySet(vals)
 }
 
 module.exports = {
-    set,
-    symmetricDifference
+    set
 }
---
+++ tests/test-set.js
@@ -0,0 +1,50 @@
+const {
+    set
+} = require("../src/set")
+const { test } = require("node:test")
+const assert = require("assert").strict
+
+test("set", () => {
+    assert.deepEqual(
+        set(1,1,2,3,4,5,5).vals(), 
+        set(1,2,3,4,5).vals()
+    )
+})
+
+test("has", () => {
+    assert.deepEqual(set(1,2,3).has(1), true)
+    assert.deepEqual(set(1,2,3).has(5), false)
+})
+
+test("union", () => {
+    assert.deepEqual(
+        set(1,2,3,4).union(set(4,5,6,7,8)).vals(),
+        set(1,2,3,4,5,6,7,8).vals()
+    )
+})
+
+test("intersection", () => {
+    assert.deepEqual(
+        set(1,2,3,4).intersection(set(4,5,6,7,8)).vals(),
+        set(4).vals()
+    )
+})
+
+test("delete", () => {
+    assert.deepEqual(set(1,2,3).delete(1).vals(), set(2,3).vals())
+    assert.deepEqual(set(1,2,3).delete(5).vals(), set(1,2,3).vals())
+})
+
+test("difference", () => {
+    assert.deepEqual(
+        set(1,2,3,4).difference(set(4,5,6,7,8)).vals(),
+        set(1,2,3).vals()
+    )
+})
+
+test("symmetricDifference", () => {
+    assert.deepEqual(
+        set(1,3,8).symmetricDifference(set(1,2,4,8)),
+        set(2,3,4)
+    )
+})
